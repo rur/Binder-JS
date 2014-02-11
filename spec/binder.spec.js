8caf1a66ff91a3d190db92508cea3488dcb1b17f
Ruaidhri Devery <info@fluid.ie>
2014-02-10 17:11:21 -0800

    made Binder#compile also work with a promise rather than assigned handler

--- spec/binder.spec.js
+++ spec/binder.spec.js
@@ -3,19 +3,26 @@ var path = require("path");
 
 
 describe("binder", function() {
-  var binder;
+  var binder, getFailSpy;
   beforeEach(function() {
     binder = new b.Binder();
+    getFailSpy = function (test, msg, done) {
+      var spy = jasmine.createSpy("FAIL");
+      spy.andCallFake(function () {
+        test.fail(msg);
+        if (typeof done === "function") done();
+      });
+      return spy;
+    }
   });
 
   it("should parse an individual file", function (done) {
     binder.compile({
-      path: path.resolve(__dirname, "fixtures/test.txt"),
-      handler: function (errors, data) {
-        expect(data).toEqual("this is a test file");
-        done();
-      }
-    });
+      path: path.resolve(__dirname, "fixtures/test.txt")
+    }).then(function (data) {
+      expect(data).toEqual("this is a test");
+      done();
+    }, getFailSpy(this, "promise was rejected", done));
   });
 
   xit("should parse a file in a directory", function(done) {
