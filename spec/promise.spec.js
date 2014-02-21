f67fb868bad21b542ca667180ebda34ada99e723
Ruaidhri Devery <info@fluid.ie>
2014-02-20 23:24:02 -0800

    created promise chain method

--- spec/promise.spec.js
+++ spec/promise.spec.js
@@ -74,4 +74,44 @@ describe("promise", function() {
       });
     });
   });
+
+  describe("promise#chain", function() {
+    var ctrl, ctrl2, p1Data, finalProm;
+    beforeEach(function() {
+      ctrl2 = p1Data = null;
+      ctrl = new p.PromiseCtrl("Test Promise");
+      finalProm = ctrl.promise.chain(function (data) {
+        p1Data = data;
+        ctrl2 = new p.PromiseCtrl("Sub Test Promise");
+        return ctrl2.promise;
+      });
+    });
+
+    it("should chain two promises together", function(done) {
+      ctrl.resolve("test");
+      ctrl2.resolve(p1Data+" plus sub");
+      finalProm.then(function (data) {
+        expect(data).toEqual("test plus sub");
+        done();
+      });
+    });
+
+    it("should reject from origin", function(done) {
+      ctrl.reject("test");
+      finalProm.then(null, function (data) {
+        expect(data).toEqual("test");
+        done();
+      });
+    });
+
+    it("should reject from second", function(done) {
+      ctrl.resolve("test");
+      ctrl2.reject(p1Data + " was rejected the second time");
+      finalProm.then(null, function (data) {
+        expect(data).toEqual("test was rejected the second time");
+        done();
+      })
+    });
+  });
+
 });
