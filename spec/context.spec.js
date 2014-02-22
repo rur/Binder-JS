1c8e0bea6af2e24fa929caaf081037b0c1d1bf8f
Ruaidhri Devery <info@fluid.ie>
2014-02-21 23:51:41 -0800

    Renamed DefaultContext to Context and worked up the implementation of the #dup method

--- spec/context.spec.js
+++ spec/context.spec.js
@@ -1,16 +1,50 @@
 var c = require("../lib/context");
 
-describe(c.DefaultContext, function() {
+describe("c.Context", function() {
   var cxt;
   beforeEach(function() {
-    cxt = new c.DefaultContext();
+    cxt = new c.Context();
+    cxt.test = "value";
   });
 
   it("should have an array of filters", function() {
-    expect(cxt.filters).toEqual([]);
+    expect(cxt.filters).toEqual(jasmine.any(Array));
   });
 
   it("should have an array of parsers", function() {
-    expect(cxt.parsers).toEqual([]);
+    expect(cxt.parsers).toEqual(jasmine.any(Array));
+  });
+
+  describe("#dup", function() {
+    var dup;
+    beforeEach(function() {
+      dup = cxt.dup();
+    });
+
+    it("should create Context instance", function() {
+      expect(dup).toEqual(jasmine.any(c.Context));
+    });
+
+    it("should copy the filters array", function() {
+      expect(dup.filters[0]).toBe(cxt.filters[0]);
+    });
+
+    it("should allow the filters to be appended to independently", function() {
+      dup.filters.push("test");
+      expect(cxt.filters).not.toContain("test");
+    });
+
+    it("should copy the parsers array", function() {
+      expect(dup.parsers[0]).toBe(cxt.parsers[0]);
+    });
+
+    it("should allow the parsers to be appended to independently", function() {
+      dup.parsers.push("test");
+      expect(cxt.parsers).not.toContain("test");
+    });
+
+    it("should copy fields applied", function() {
+      expect(dup.test).toEqual("value");
+    });
   });
 });
