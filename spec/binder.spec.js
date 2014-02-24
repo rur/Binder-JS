e11523d57d41628c74e51692ffac483ffd9f507d
Ruaidhri Devery <info@fluid.ie>
2014-02-23 17:13:52 -0800

    Removed default filter and parser definitions from the code into the specs

--- spec/binder.spec.js
+++ spec/binder.spec.js
@@ -1,12 +1,17 @@
 var Binder = require("../lib/binder");
 var Parser = require("../lib/parser");
+var s = require("../lib/scanner");
 var path = require("path");
+var fs = require("fs");
 
-
+/*
+  Binder Jasmine Specs
+ */
 describe("binder", function() {
   var binder;
   beforeEach(function() {
     binder = new Binder();
+    configForTests(binder);
   });
 
   describe("compiling a single file", function() {
@@ -111,5 +116,88 @@ describe("binder", function() {
     });
 
   });
-
 });
+
+///////////////////
+//// Spec Helpers
+//
+// Add filters and parers for tests
+function configForTests (binder) {
+  // create file property in context
+  binder.filter(function (pth, cxt) {
+    fs.stat(pth, this.handle(function (err, stats) {
+      if (err) {
+        this.reject(err);
+      } else {
+        cxt.file = {
+          isDir: stats.isDirectory(),
+          ext: path.extname(pth),
+        };
+        this.resolve();
+      }
+    }));
+    return this.promise;
+  });
+  // Null Parser
+  binder.parse(function () {
+      return true;
+    }, function () {
+      this.reject(); // skip
+    });
+  // UTF Parser
+  binder.parse(function (_, cxt) {
+      var fileTypes = [".txt", ".js"];
+      return fileTypes.indexOf(cxt.file.ext) > -1;
+    }, function (pth) {
+      fs.readFile(pth, "utf-8", this.handle(function (err, data) {
+        if (err) {
+          this.reject(err);
+          return;
+        }
+        this.resolve(data);
+      }));
+      return this.promise;
+    });
+  // Folder Parser
+  binder.parse(function (_, cxt) {
+      return cxt.file.isDir;
+    }, function (pth, cxt) {
+      var parser = this;
+      fs.readdir(pth, this.handle(function (err, files) {
+        var count, fileData = {};
+        var subCxt, file;
+        // handlers
+        function makeFileResolver(file, f_cxt) {
+          return parser.handle(function (data) {
+            fileData[file] = data;
+            if (--count === 0) {
+              this.resolve(fileData);
+            }
+          });
+        }
+        function makeFileRejector(file, f_cxt) {
+          return parser.handle(function (err) {
+            count--;
+            if (err !== void 0) {
+              this.reject(err);
+            }
+          });
+        }
+        //
+        if (err) {
+          this.reject(err);
+        } else {
+          count = files.length;
+          for (var i=0, len=count; i < len; i++) {
+            subCxt = cxt.dup();
+            file = files[i];
+            s.scanFile(path.join(pth, file), subCxt).then(
+              makeFileResolver(file, subCxt),
+              makeFileRejector(file, subCxt)
+            );
+          }
+        }
+      }));
+      return this.promise;
+    });
+}
