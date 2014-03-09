c4602c38de534375638227891ad65edf7d83de08
Ruaidhri Devery <info@fluid.ie>
2014-03-09 16:15:06 -0700

    added definition dependency resolution to index#define method

--- index.js
+++ index.js
@@ -2,16 +2,55 @@ var Definition = require("./lib/definition");
 var Binder = require("./lib/binder");
 var Rule = require("./lib/rule");
 
+var definitions = {};
+
 var binder_js_api = {
-  define: function (name) {
-    return new Definition(name);
+  /**
+   * Create a new binder syntax definition
+   *
+   * @param  {string}         name      The unique name of this Binder
+   * @param  {array<string>}  depNames  Base definitions to extend
+   * @return {Definition}   Binder definition object with which you can
+   *                               now define syntax rules
+   */
+  define: function (name, depNames) {
+    var deps = [], dep, dName;
+    if (depNames instanceof Array) {
+      for (var i = 0; i < depNames.length; i++) {
+        dName = depNames[i];
+        if (!definitions.hasOwnProperty(dName)) {
+          throw "Binder definition '"+dName+"' not found";
+        }
+        deps.push(definitions[dName]);
+      }
+    }
+    var def = definitions[name] = new Definition(name, deps);
+    return def;
   },
+  /**
+   * Create an instance of a binder specifying the name of the
+   * syntax definition to use.
+   *
+   * You now attached new filters and parsers.
+   *
+   * @param  {string} defName Binder syntax definition name, 'default' if undefined
+   * @return {Binder}         The binder instance that implements
+   *                              rules for attaching data handlers
+   */
   create: function (defName) {
     return new Binder();
   },
+  /**
+   * Create rule object to defined new parse rules for this context and
+   * sub contexts.
+   *
+   * @param  {Context} cxt The context instance to add the rules too
+   * @return {Rule}     Binder rule
+   */
   rule: function (cxt) {
     return new Rule(cxt);
   }
 };
+
 
 module.exports = binder_js_api;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -7,8 +7,18 @@ describe("define", function() {
   it("should create a new definition object", function() {
     expect(index.define("something")).toEqual(jasmine.any(Definition));
   });
-});
 
+  it("should provide the definition with a ref to dep", function() {
+    var dep = index.define("depDef");
+    expect(index.define("test", ["depDef"]).deps).toContain(dep);
+  });
+
+  it("should throw an error if the base definition name doesn't exist", function() {
+    expect(function () {
+      index.define("test", ["typo"]);
+    }).toThrow("Binder definition 'typo' not found");
+  });
+});
 
 describe("create", function() {
   it("should create a binder instance", function() {
