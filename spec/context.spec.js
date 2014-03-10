cd358e2420831ca961d041a2c194e13afa44c3c0
Ruaidhri Devery <info@fluid.ie>
2014-03-09 22:10:35 -0700

    Implemented the Context#createParserExpr for most basic usage

--- spec/context.spec.js
+++ spec/context.spec.js
@@ -1,4 +1,5 @@
 var Context = require("../lib/context");
+var Syntax = require("../lib/syntax");
 
 describe("Context", function() {
   var cxt;
@@ -45,6 +46,77 @@ describe("Context", function() {
 
     it("should copy fields applied", function() {
       expect(dup.test).toEqual("value");
+    });
+  });
+
+  describe("#createParserExpr", function() {
+    var expr, syx;
+    beforeEach(function() {
+      syx = new Syntax;
+      cxt._syntax = syx;
+    });
+
+    describe("adding basic parser", function() {
+      function cond () {};
+      function parse () {};
+      beforeEach(function() {
+        expr = cxt.createParserExpr();
+        expr.when(cond).then(parse);
+      });
+
+      it("should add a new parser", function() {
+        expect(cxt.parsers.length).toEqual(1);
+      });
+
+      it("should have added the condition to the parser", function() {
+        expect(cxt.parsers[0].condition).toBe(cond);
+      });
+
+      it("should have added the parse handler to the parser", function() {
+        expect(cxt.parsers[0].parse).toBe(parse);
+      });
+
+      it("should error without a condition", function() {
+        expect(function() {
+          expr = cxt.createParserExpr();
+          expr.then();
+        }).toThrow("Cannot add a parser without a condition");
+      });
+    });
+
+    describe("multiple conditions", function() {
+      var first, second;
+      beforeEach(function() {
+        expr = cxt.createParserExpr();
+        first = jasmine.createSpy("first");
+        second = jasmine.createSpy("second");
+        expr.when(first).when(second).then(function() {});
+      });
+
+      it("should still create a parser", function() {
+        expect(cxt.parsers.length).toEqual(1);
+      });
+
+      it("should call the first", function() {
+        cxt.parsers[0].condition("path", cxt);
+        expect(first).wasCalledWith("path", cxt);
+      });
+
+      it("should call the second", function() {
+        first.andReturn(true);
+        cxt.parsers[0].condition("path", cxt);
+        expect(second).wasCalledWith("path", cxt);
+      });
+
+      it("should be true", function() {
+        first.andReturn(true);
+        second.andReturn(true);
+        expect(cxt.parsers[0].condition("path", cxt)).toBeTruthy();
+      });
+    });
+
+    describe("defined syntax", function() {
+
     });
   });
 });
