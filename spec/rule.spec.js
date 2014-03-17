c3691b645cd5f13cf4027c52b4f97af3d0cbd0fc
Ruaidhri Devery <info@fluid.ie>
2014-03-16 22:23:55 -0700

    updated rule spec to test current implementation

--- spec/rule.spec.js
+++ spec/rule.spec.js
@@ -1,53 +1,38 @@
 var Rule = require("../lib/rule");
 
-/////////////////////////////////////////////////////////
-// NB:  The following is a stub implementation
-//      It will be replaced once the Definition and
-//      Syntax implementations are worked out
-/////////////////////////////////////////////////////////
-
-xdescribe("rule", function() {
-  var rule, cxt, parsers, filters;
+describe("rule", function() {
+  var rule, cxt, expr;
   beforeEach(function() {
-    parsers = [];
-    filters = [];
+    expr = {
+      when: jasmine.createSpy("test condition"),
+      parseFile: jasmine.createSpy("read parse handler")
+    };
+    expr.when.andReturn(expr);
+    expr.parseFile.andReturn(expr);
     cxt = {
-      filters: filters,
-      parsers: parsers
+      filters: [],
+      createParserExpr: function () {
+        return expr;
+      },
     };
     rule = new Rule(cxt);
   });
 
-  it("should assign a context to itself", function() {
-    expect(rule.context).toBe(cxt);
-  });
 
   describe("#parse", function() {
-    var condition, parse;
-    beforeEach(function() {
-      condition = function() {};
-      parse = function() {};
+    function condit () {}
+    function parse () {}
+
+    it("should complete the expression", function() {
+      rule.parse(condit, parse);
+      expect(expr.when).wasCalledWith(condit);
+      expect(expr.parseFile).wasCalledWith(parse);
     });
 
-    it("should register a new parser with the context", function() {
-      rule.parse(condition, parse);
-
-      expect(parsers[0].condition).toBe(condition);
-      expect(parsers[0].parse).toBe(parse);
-    });
-
-    it("should allow you to add a read function manually", function() {
-      rule.parse(condition).read(parse);
-
-      expect(parsers[0].condition).toBe(condition);
-      expect(parsers[0].parse).toBe(parse);
-    });
-
-    it("should allow you to define a predicate and task manually", function() {
-      rule.parse.when(condition).read(parse);
-
-      expect(parsers[0].condition).toBe(condition);
-      expect(parsers[0].parse).toBe(parse);
+    it("should complete the expression using statement syntax", function() {
+      rule.parse.when(condit).parseFile(parse);
+      expect(expr.when).wasCalledWith(condit);
+      expect(expr.parseFile).wasCalledWith(parse);
     });
   });
 
@@ -55,27 +40,7 @@ xdescribe("rule", function() {
     it("should add a filter", function() {
       function filter () {}
       rule.filter(filter);
-      expect(filters[0]).toBe(filter);
+      expect(cxt.filters[0]).toBe(filter);
     });
-  });
-
-  describe("#definePreParser", function() {
-    var spy, parse;
-    beforeEach(function() {
-      rule.definePreParser("read_test", function () {
-        return "abc";
-      });
-      spy = jasmine.createSpy("Parser").andReturn("123");
-      rule.parse(function () { return true; }).read_test(spy);
-      parse = parsers[0];
-    });
-
-    it("should add a pre parser method", function() {
-      expect(parse()).toEqual("123");
-    });
-  });
-
-  xdescribe("#definePredicate", function() {
-
   });
 });
