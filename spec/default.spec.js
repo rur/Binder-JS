9866914461b0fe385fee08f174776f3dd694899b
Ruaidhri Devery <info@fluid.ie>
2014-03-24 22:32:40 +0000

    Added context route filter

--- spec/default.spec.js
+++ spec/default.spec.js
@@ -4,25 +4,25 @@ var Definition = require("../lib/definition");
 var Context = require("../lib/context");
 var Binder = require("../lib/binder");
 
-describe("addDefaultDefinition", function() {
+describe("addDefaultDefinition", function () {
   var def;
-  beforeEach(function() {
+  beforeEach(function () {
     def = new Definition("default");
     d_fault(def);
   });
 
-  it("should add an ignore parse handler", function() {
+  it("should add an ignore parse handler", function () {
     expect(def.parsers.ignore.name).toEqual("ignore");
   });
 
-  it("should have an always condition", function() {
+  it("should have an always condition", function () {
     expect(def.conditions.always.name).toEqual("always");
   });
 });
 
-describe("parsing", function() {
+describe("parsing", function () {
   var binder, res, rej, cxt;
-  beforeEach(function() {
+  beforeEach(function () {
     cxt = new Context();
     var def = new Definition();
     d_fault(def);
@@ -34,22 +34,54 @@ describe("parsing", function() {
     rej = jasmine.createSpy("Reject Handler");
   });
 
-  describe("defined filters", function() {
-    it("should get the file info", function(done) {
-      var spy = jasmine.createSpy("Filter Spy");
-      spy.andCallFake(function (pth, cxt) {
-        done();
-        expect(cxt.file.ext).toEqual(".skip");
-        expect(cxt.file.name).toEqual("someFile.skip");
-        expect(cxt.file.path).toContain("/fixtures/nestedData/someFile.skip");
+  describe("defined filters", function () {
+    describe("file info", function () {
+      it("should get the file info", function (done) {
+        var spy = jasmine.createSpy("Filter Spy");
+        spy.andCallFake(function (pth, cxt) {
+          done();
+          expect(cxt.file.ext).toEqual(".skip");
+          expect(cxt.file.name).toEqual("someFile.skip");
+          expect(cxt.file.path).toContain("/fixtures/nestedData/someFile.skip");
+        });
+        binder.filter(spy);
+        binder.compile(path.resolve(__dirname, "fixtures/nestedData/someFile.skip"));
       });
-      binder.filter(spy);
-      binder.compile(path.resolve(__dirname, "fixtures/nestedData/someFile.skip"));
     });
 
-    it("should handle errors in defined filters", function(done) {
+    describe("route", function () {
+      it("should start as an empty array", function (done) {
+        binder.filter(function (fp, cx) {
+          done();
+          expect(cx.route).toEqual([]);
+        });
+        binder.compile(path.resolve(__dirname, "fixtures/nestedData/someFile.skip"));
+      });
+
+      it("should add a dir name to the route", function (done) {
+        binder.filter(function (fp, cx) {
+          if (cx.file.name == "subDir") {
+            done();
+            expect(cx.route).toEqual(["subDir"]);
+          }
+        });
+        binder.compile(path.resolve(__dirname, "fixtures/nestedData/"));
+      });
+
+      it("should add manage route in sub folders children", function (done) {
+        binder.filter(function (fp, cx) {
+          if (cx.file.name === "siblingRoute.txt") {
+            done();
+            expect(cx.route.join("/")).toEqual("otherSubDir/sibling copy 2");
+          }
+        });
+        binder.compile(path.resolve(__dirname, "fixtures/nestedData/"));
+      });
+    });
+
+    it("should handle errors in defined filters", function (done) {
       var err = new Error("test error");
-      binder.filter(function() {
+      binder.filter(function () {
         throw err;
       });
       binder.compile(path.resolve(__dirname, "fixtures/nestedData/someFile.skip")).then(null, function (reason) {
@@ -59,8 +91,8 @@ describe("parsing", function() {
     });
   });
 
-  describe("Null Parser", function(done) {
-    it("should ignore a file it doesn't recognize", function(done) {
+  describe("Null Parser", function (done) {
+    it("should ignore a file it doesn't recognize", function (done) {
       binder.compile(path.resolve(__dirname, "fixtures/nestedData/someFile.skip")).then(
         getFailSpy(this, done, "resolve"),
         function (reason) {
@@ -70,16 +102,16 @@ describe("parsing", function() {
     });
   });
 
-  describe(".txt Parser", function() {
+  describe(".txt Parser", function () {
     it("should parse a txt file", function (done) {
       var spec = this;
-      binder.compile(path.resolve(__dirname, "fixtures/test.txt")).then(function(reason) {
+      binder.compile(path.resolve(__dirname, "fixtures/test.txt")).then(function (reason) {
         done();
         expect(reason).toEqual("this is a test file");
       }, getFailSpy(this, done, "reject"));
     });
 
-    it("should allow a new rule be defined with an after handler", function(done) {
+    it("should allow a new rule be defined with an after handler", function (done) {
       binder.parse.fileExt(".skip").readUTF(function (data) {
         return data + "; until this custom parser was added!";
       });
@@ -90,8 +122,8 @@ describe("parsing", function() {
     });
   });
 
-  describe("folder parser", function() {
-    it("should parse a folders data", function(done) {
+  describe("folder parser", function () {
+    it("should parse a folders data", function (done) {
       binder.compile(path.resolve(__dirname, "fixtures/simpleDir/")).then( function (data) {
         done();
         expect(data).toEqual({
@@ -100,20 +132,20 @@ describe("parsing", function() {
       }, getFailSpy(this, done, "reject"));
     });
 
-    describe("nested data", function() {
+    describe("nested data", function () {
       var compiled;
-      beforeEach(function() {
+      beforeEach(function () {
         compiled = binder.compile(path.resolve(__dirname, "fixtures/nestedData/"));
       });
 
-      it("should parse a nested dir", function(done) {
+      it("should parse a nested dir", function (done) {
         compiled.then(function (data) {
                 done();
                 expect(data.subDir).toEqual({"test.txt": "this is another text file inside a sub directory"});
               }, getFailSpy(this, done, "reject"));
       });
 
-      it("should skip unknown file type", function(done) {
+      it("should skip unknown file type", function (done) {
         compiled.then(function (data) {
                 done();
                 expect(data["someFile.skip"]).toBeUndefined();
