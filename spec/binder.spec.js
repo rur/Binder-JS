69dca5f5f1c52a7dafb5b6cdeb764dd7323787b4
Ruaidhri Devery <info@fluid.ie>
2014-07-01 23:50:04 -0700

    moved Parser rule creation responsibility into Binder

--- spec/binder.spec.js
+++ spec/binder.spec.js
@@ -1,10 +1,28 @@
 var Binder = require("../lib/binder");
+var Syntax = require("../lib/syntax");
+var Parser = require("../lib/parser");
 var scanner = require("../lib/scanner");
+var proc = require("../lib/proc");
 
 describe("Binder", function() {
-  var binder;
+  var binder, syntax;
+
   beforeEach(function() {
-    binder = new Binder({mock: "context", filters: [], parsers: []});
+    syntax = new Syntax();
+    syntax.parsers.action = {
+      name: "action",
+      proc: proc(function () {})
+    };
+    syntax.conditions.test = {
+      name: "test",
+      proc: proc(function () {})
+    };
+    binder = new Binder({
+      mock: "context",
+      filters: [],
+      parsers: [],
+      _syntax: syntax
+    });
   });
 
   it("should create a binder", function() {
@@ -22,31 +40,64 @@ describe("Binder", function() {
     expect(binder.context.filters).toContain(mock);
   });
 
-  it("should create parser expressions", function() {
-    var exp = {
-      someCondition: jasmine.createSpy("someCondition"),
-      someParse: jasmine.createSpy("someParse")
-    };
-    exp.someCondition.andReturn(exp);
-    exp.someParse.andReturn(exp);
-    binder.context.createParserExpr = function () {
-      return exp;
-    }
-    binder.parse.someCondition("abc").someParse("def");
-    expect(exp.someCondition).wasCalledWith("abc");
-    expect(exp.someParse).wasCalledWith("def");
+  it("should have begin a parse statement", function () {
+    expect(binder.parse.test).toEqual(jasmine.any(Function));
   });
 
   describe("#compile", function() {
     var dup;
     beforeEach(function() {
-      binder.compileTimeout = 123;
       spyOn(scanner, "scan");
     });
 
     it("should call scan", function() {
+      binder.compileTimeout = 123;
       binder.compile("some/path");
       expect(scanner.scan).wasCalledWith(binder.context, "some/path", 123);
+    });
+
+    describe("parsers", function () {
+      var parser;
+
+      beforeEach(function () {
+        // define parser
+        binder
+          .parse
+            .test(1,2, function testFn() {})
+            .action(function actionFn() {});
+        binder.compile("something");
+        parser = binder.context.parsers[0];
+      });
+
+      it("should create a parser", function () {
+        expect(parser).toEqual(jasmine.any(Parser));
+      });
+
+      it("should have added the condition proc", function () {
+        expect(parser.condition.length).toEqual(2);
+      });
+
+      it("should have added the parser proc", function () {
+        expect(parser.parse.length).toEqual(2);
+      });
+
+      it("should catch an invalid expression", function () {
+        binder.parse.action;
+        expect(function () {
+          binder.compile("FAIL");
+        }).toThrow("Cannot compile parser, invalid rule statement[0]: 'action'");
+      });
+
+      it("should catch an invalid word", function () {
+        binder.parse.action.test;
+        expect(function () {
+          binder.compile("FAIL");
+        }).toThrow("Cannot compile parser, invalid rule statement[0]: 'action test'");
+      });
+
+      it("should add default params to procs", function () {
+        expect(parser.condition.defaultParams).toEqual([undefined, undefined, 1, 2]);
+      });
     });
   });
 });
