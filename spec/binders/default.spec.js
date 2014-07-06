d222e044219ab3532ad0cb1be2a67ddb141c0c04
Ruaidhri Devery <info@fluid.ie>
2014-07-06 15:07:59 -0700

    added glob style matching to the default route condition

--- spec/binders/default.spec.js
+++ spec/binders/default.spec.js
@@ -16,7 +16,55 @@ describe("binders/default", function () {
     });
 
     it("should match a route condition", function () {
-      expect(def.conditions.route.func({route: ["test", "abc"] }, "abc", "test/abc")).toBe(true);
+      expect(def.conditions.route.func("abc", {route: ["test", "abc"] }, "test/*")).toBe(true);
     });
+  });
+
+  describe("in a binder", function () {
+    var binder;
+    beforeEach(function () {
+      var index = require("../../index");
+      var def = index.define('test');
+      def.parser("test", function () {
+        return "test parser";
+      });
+      binder = index(def);
+    });
+
+    it("should have ignore command in the syntax", function () {
+      expect(binder.parse.ignore).toEqual(jasmine.any(Function));
+    });
+
+    it("should work", function (done) {
+      binder.parse.always().test();
+      binder.compile("abc").then(function (data) {
+        expect(data).toEqual("test parser");
+        done();
+      }, done);
+    });
+
+    describe("route", function () {
+      beforeEach(function () {
+        binder.context.route = ["a", 'b'];
+      });
+
+      it("should match a route glob", function (done) {
+        binder.parse.route('**/c').test();
+        binder.compile("a/b/c", "c").then(function (data) {
+          expect(data).toEqual("test parser");
+          done();
+        }, done);
+      });
+
+      it("should chain route with another condition", function (done) {
+        var spy = jasmine.createSpy().andReturn(true);
+        binder.parse.route('a/b/c', spy).always().test();
+        binder.compile("a/b/c", "c").then(function (data) {
+          expect(spy).wasCalledWith("a/b/c", jasmine.any(Object));
+          done();
+        }, done);
+      });
+    });
+
   });
 });
