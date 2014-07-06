7acf71bb33a228384acbbbdc92ef7889d74e4c67
Ruaidhri Devery <info@fluid.ie>
2014-07-06 00:32:59 -0700

    move test coverage of binder_js index function

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,53 +3,6 @@ var Binder = require("../lib/binder");
 var Context = require("../lib/context");
 var index = require("../index");
 
-xdescribe("index#create", function() {
-  var binder;
-  beforeEach(function() {
-    index.define("test")
-      .parser("readTest", function (path) {
-        return path + " test read";
-      })
-      .condition("truthy", function () {
-        return true;
-      })
-      .init(function(binder) {
-        binder.parse.truthy().readTest();
-      });
-    binder = index.create("test");
-  });
-
-  it("should create a binder instance", function() {
-    expect(binder).toEqual(jasmine.any(Binder));
-  });
-
-  it("should create a basic binder", function () {
-    expect(index.create()).toEqual(jasmine.any(Binder));
-  });
-
-  it("have added a parsers condition term", function() {
-    expect(binder.parse.truthy).toEqual(jasmine.any(Function));
-  });
-
-  it("should have added parser handler term", function() {
-    expect(binder.parse.readTest).toEqual(jasmine.any(Function));
-  });
-
-  it("should error if a specified definition wasn't found", function() {
-    expect(function () {
-      index.create("unknown");
-    }).toThrow("No binder definition was found with the name: 'unknown'");
-  });
-
-  it("should apply definition init", function() {
-    expect(binder.context.parsers.length).toBeGreaterThan(0);
-  });
-
-  it("should have default definition applied", function() {
-    expect(binder.parse.ignore).toEqual(jasmine.any(Function));
-  });
-});
-
 describe("jsBidner.define", function () {
   it("should create a definition object", function () {
     expect(index.define('test')).toEqual(jasmine.any(Definition));
@@ -62,15 +15,60 @@ describe("jsBidner.define", function () {
 });
 
 describe("jsBinder", function() {
-  it("should create a binder from a context", function() {
-    expect(index(new Context)).toEqual(jasmine.any(Binder));
+  describe("create from definition", function () {
+    var binder;
+    beforeEach(function() {
+      var def = index.define("test");
+      def.parser("readTest", function (path) {
+          return path + " test read";
+        })
+        .condition("truthy", function () {
+          return true;
+        })
+        .init(function(binder) {
+          binder.parse.truthy().readTest();
+        });
+      binder = index(def);
+    });
+
+    it("should create a binder instance", function() {
+      expect(binder).toEqual(jasmine.any(Binder));
+    });
+
+    it("have added a parsers condition term", function() {
+      expect(binder.parse.truthy).toEqual(jasmine.any(Function));
+    });
+
+    it("should have added parser handler term", function() {
+      expect(binder.parse.readTest).toEqual(jasmine.any(Function));
+    });
+
+    it("should error if a specified definition wasn't found", function() {
+      expect(function () {
+        index("unknown");
+      }).toThrow("Binder setup must either be a definition or the context from another binder process. Given: (unknown)");
+    });
+
+    it("should apply definition init", function() {
+      expect(binder.statements.length).toBeGreaterThan(0);
+    });
+
+    xit("should have default definition applied", function() {
+      expect(binder.parse.ignore).toEqual(jasmine.any(Function));
+    });
   });
 
-  it("should dup the context", function () {
-    var cxt = new Context();
-    var dupCxt = new Context();
-    spyOn(cxt, 'dup').andReturn(dupCxt);
-    expect(index(cxt).context).toBe(dupCxt);
-    expect(cxt.dup).wasCalled();
+  describe("create from context", function () {
+    it("should create a binder from a context", function() {
+      expect(index(new Context)).toEqual(jasmine.any(Binder));
+    });
+
+    it("should dup the context", function () {
+      var cxt = new Context();
+      var dupCxt = new Context();
+      spyOn(cxt, 'dup').andReturn(dupCxt);
+      expect(index(cxt).context).toBe(dupCxt);
+      expect(cxt.dup).wasCalled();
+    });
   });
 });
