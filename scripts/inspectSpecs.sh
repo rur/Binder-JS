e2f80f4766897f738721162514cf357549d10540
Ruaidhri Devery <info@fluid.ie>
2014-07-06 15:09:10 -0700

    housekeeping for test setup

--- scripts/inspectSpecs.sh
+++ scripts/inspectSpecs.sh
@@ -1,1 +1,5 @@
-node --debug-brk /usr/local/lib/node_modules/jasmine-node/lib/jasmine-node/cli.js .
+#!/bin/bash
+
+BASE_DIR=`dirname $0`
+
+node --debug-brk $BASE_DIR/../node_modules/jasmine-node/lib/jasmine-node/cli.js .
--- spec/_spec_helper.js
+++ spec/_spec_helper.js
@@ -1,6 +1,6 @@
 exports.getFailSpy = function (test, done, name) {
   return function () {
-    test.fail("'" + name + "' handler should not have been called. Args: [" + Array.prototype.slice.call(arguments).join(",") + "]");
+    test.fail("'" + name + "' handler should not have been called. Args: (" + Array.prototype.slice.call(arguments).join(",") + ")");
     if (typeof done === "function") done();
   };
 };
